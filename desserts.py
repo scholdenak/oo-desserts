c99423010438e12d29e692c1403b17a0070a8da1
vagrant <vagrant@vagrant.vm>
2020-10-13 03:27:52 +0000

    Worked on instance methods,

--- desserts.py
+++ desserts.py
@@ -3,7 +3,30 @@
 
 class Cupcake:
     """A cupcake."""
+    cache = {}
 
+    def __init__(self, name, flavor, price):
+        self.name = name
+        self.flavor = flavor
+        self.price = price
+        self.qty = 0
+
+        self.cache[self.name] = [self.flavor, self.price]
+
+    def add_stock(self, amount):
+        self.qty = self.qty + amount
+        return self.qty
+
+    def sell(self, amount):
+        while (self.qty - amount) >= 0:
+            if self.qty == 0:
+                print('Sorry, these cupcakes are sold out')
+            else:
+                self.qty -= amount
+            else:
+                print("nope")
+            break
+        
     def __repr__(self):
         """Human-readable printout for debugging."""
 
